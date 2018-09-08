817458f894f501943bd6c58cb2847fb707bc6b43
Juan García Garland <jpgarcia@fing.edu.uy>
2018-09-08 13:12:15 -0300

    all datatype modules refactored, AspectAG remaining

---
+++ src/Attribution.hs
@@ -0,0 +1,141 @@
+{-|
+Module      : Attribution
+Description : Maps from attribute names to attribute values
+Copyright   : (c) Juan García Garland, 2018 
+License     : LGPL
+Maintainer  : jpgarcia@fing.edu.uy
+Stability   : experimental
+Portability : POSIX
+
+Used to build attributions, which are mappings from labels to values
+-}
+
+{-# LANGUAGE DataKinds,
+             TypeOperators,
+             PolyKinds,
+             GADTs,
+             TypeInType,
+             RankNTypes,
+             StandaloneDeriving,
+             FlexibleInstances,
+             FlexibleContexts,
+             ConstraintKinds,
+             MultiParamTypeClasses,
+             FunctionalDependencies,
+             UndecidableInstances,
+             ScopedTypeVariables,
+             TypeFamilies
+#-}
+
+module Attribution where
+
+import Data.Kind 
+import Data.Type.Equality
+import Data.Proxy
+import Attribute
+import TPrelude
+import TagUtils
+
+
+-- | Internal representation, isomorphic to a 'Record'
+data Attribution :: forall k . [(k,Type)] -> Type where
+  EmptyAtt :: Attribution '[]
+  ConsAtt  :: LabelSet ( '(att, val) ': atts) =>
+   Attribute att val -> Attribution atts -> Attribution ( '(att,val) ': atts)
+
+
+-- * Pretty constructors
+
+-- | Extending
+infixr 2 *.
+(*.) :: LabelSet ('(att, val) : atts) =>
+    Attribute att val -> Attribution atts -> Attribution ('(att, val) : atts)
+(*.) = ConsAtt
+
+-- | Empty
+emptyAtt :: Attribution '[]
+emptyAtt = EmptyAtt
+
+-- | A getter, also works as a predicate
+class HasFieldAtt (l::k) (r :: [(k,Type)]) v | l r -> v where
+   lookupByLabelAtt:: Label l -> Attribution r -> v
+
+instance (HEqK l l1 b, HasFieldAtt' b l ( '(l1,v1) ': r) v)
+    => HasFieldAtt l ( '(l1,v1) ': r) v where
+    lookupByLabelAtt l (r :: Attribution ( '(l1,v1) ': r)) =
+         lookupByLabelAtt' (Proxy::Proxy b) l r
+
+
+class HasFieldAtt' (b::Bool) (l :: k) (r::[(k,Type)]) v | b l r -> v where
+    lookupByLabelAtt':: Proxy b -> Label l -> Attribution r -> v
+
+instance HasFieldAtt' True l ( '(l,v) ': r) v where
+   lookupByLabelAtt' _ _ (ConsAtt (Attribute v) _) = v
+instance HasFieldAtt l r v => HasFieldAtt' False l ( '(l2,v2) ': r) v where
+   lookupByLabelAtt' _ l (ConsAtt _ r) = lookupByLabelAtt l r
+
+
+
+
+--UpdateAtLabel
+--I attempt to code an indexed type implementation, where the resulting Type
+--function of the parameters.
+--I could code the type function over the type level,
+--the problem is when I do this on a type class to code ter level computations.
+--Since we decide from the context (HEqK ) the returned boolean must be a
+--parameter of UpdateAtLabelR, but since it's purely on the context,
+--it is free on the RHS...
+--The fundep implementation is needed..
+
+
+-- | Update an attribution at a Label, putting an attribute v.
+--Note that not only the value but also the type at the position could
+--be updated
+class UpdateAtLabelAtt (l :: k)(v :: Type)(r :: [(k,Type)])(r' :: [(k,Type)])
+   | l v r -> r' where
+  updateAtLabelAtt :: Label l -> v -> Attribution r -> Attribution r'
+
+--So we need an auxiliary class with an extra parameter to decide if we update
+--on the head of r or not
+
+class UpdateAtLabelAtt' (b::Bool)(l::k)(v::Type)(r::[(k,Type)])(r'::[(k,Type)])
+    | b l v r -> r'  where
+  updateAtLabelAtt' :: Proxy b -> Label l -> v->Attribution r -> Attribution r'
+
+
+
+instance (HEqK l l' b, UpdateAtLabelAtt' b l v ( '(l',v')': r) r')
+ -- note that if pattern over r is not written this does not compile
+       => UpdateAtLabelAtt l v ( '(l',v') ': r) r' where
+  updateAtLabelAtt = updateAtLabelAtt' (Proxy :: Proxy b)
+
+
+instance (LabelSet ( '(l,v') ': r), LabelSet ( '(l,v) ': r) ) =>
+         UpdateAtLabelAtt' 'True l v ( '(l,v') ': r) ( '(l,v) ': r) where
+  updateAtLabelAtt' _ (l :: Label l) v (att `ConsAtt` atts)
+    = (Attribute v :: Attribute l v) `ConsAtt` atts
+
+
+instance ( UpdateAtLabelAtt l v r r', LabelSet  ( a ': r' ) ) =>
+         UpdateAtLabelAtt' False l v ( a ': r) ( a ': r') where
+  updateAtLabelAtt' (b :: Proxy False) (l :: Label l) (v :: v)
+    (ConsAtt att xs :: Attribution ( a ': r))
+    = case (updateAtLabelAtt l v xs) of
+        xs' -> ConsAtt att xs' :: Attribution( a ': r')
+
+
+
+-- TODO: implement type error
+--instance Fail (FieldNotFound l) => UpdateAtLabelAtt l v '[] '[] where
+--    updateAtLabelAtt _ _ r = r
+
+
+
+
+instance Show (Attribution '[]) where
+  show _ = "«»"
+
+instance (Show val, Show (Attribution atts)) =>
+         Show (Attribution  ( '(att,val) ': atts ) ) where
+  show (ConsAtt att atts) = let tail = show atts
+                            in "«" ++ show (getVal att) ++ "," ++ drop 1 tail 
---
+++ src/InternalTest.hs
@@ -0,0 +1,202 @@
+
+
+{-# LANGUAGE DataKinds,
+             TypeOperators,
+             PolyKinds,
+             GADTs,
+             TypeInType,
+             RankNTypes,
+             StandaloneDeriving,
+             FlexibleInstances,
+             FlexibleContexts,
+             ConstraintKinds,
+             MultiParamTypeClasses,
+             FunctionalDependencies,
+             UndecidableInstances,
+             ScopedTypeVariables,
+             TypeFamilies
+#-}
+
+module InternalTest where
+import Data.Kind 
+import Data.Type.Equality
+import Data.Proxy
+import Eq
+import Record
+import Data.Tagged
+
+
+-- import Attribute
+-- import TPrelude
+
+-- import Attribution
+-- import ChildAtts
+-- import AspectAG
+-- import HList
+import TagUtils
+
+
+data Lbl = Label1 | Label2 | Label3 | Label4 deriving Show
+
+
+label1 = Label :: Label 'Label1
+label2 = Label :: Label 'Label2
+label3 = Label :: Label 'Label3
+label4 = Label :: Label 'Label4
+
+
+
+----Record
+
+tagged1 = Tagged 3   :: Tagged 'Label1 Int 
+tagged2 = Tagged '4' :: Tagged 'Label2 Char
+tagged3 = Tagged '4' :: Tagged 'Label3 Char
+
+record1 = ConsR tagged2 EmptyR
+-- test2 = ConsR att2 test1 does not compile because of label duplication
+record2 = ConsR tagged1 record1
+record3 = ConsR tagged3 record2
+
+
+
+--test_update_1 = updateAtLabelRec label4 False record3 --should fail
+test_update_R_2 = updateAtLabelRec label2 False record3 
+test_update_R_3 = updateAtLabelRec label2 "hola" record3
+test_update_R_4 = updateAtLabelRec label2 '9' record3 
+test_update_R_5 = updateAtLabelRec label3 "hola" record3 
+test_update_R_6 = updateAtLabelRec label3 '9' record3 
+
+
+
+
+
+
+
+
+
+-- --Some tests:
+
+-- ---- Attribution
+
+-- att1 = Attribute 3   :: Attribute Label1 Int 
+-- att2 = Attribute '4' :: Attribute Label2 Char
+-- att3 = Attribute '4' :: Attribute Label3 Char
+
+-- attrib1 = ConsAtt att2 EmptyAtt
+-- -- test2 = ConsAtt att2 test1 does not compile because of label duplication
+-- attrib2 = ConsAtt att1 attrib1
+-- attrib3 = ConsAtt att3 attrib2
+
+-- --test_update_1 = updateAtLabelAtt label4 False attrib3 --should fail
+-- test_update_2 = updateAtLabelAtt label2 False attrib3 
+-- test_update_3 = updateAtLabelAtt label2 "hola" attrib3
+-- test_update_4 = updateAtLabelAtt label2 '9' attrib3 
+-- test_update_5 = updateAtLabelAtt label3 "hola" attrib3 
+-- test_update_6 = updateAtLabelAtt label3 '9' attrib3 
+
+
+-- ----ChildAtts
+
+-- data LabelL; data LabelR
+-- labelL = Label :: Label (LabelL,Char)
+-- labelR = Label :: Label (LabelR,Int)
+
+
+-- childAttLR = ConsCh (TaggedChAttr labelL attrib1)$
+--              ConsCh (TaggedChAttr labelR attrib2) EmptyCh
+
+-- -- duplicatedLabel
+-- -- childAttRRFail = ConsCh (TaggedChAttr labelR attrib1)$
+-- --              ConsCh (TaggedChAttr labelR attrib2) EmptyCh
+
+-- attrib1get = hLookupByChild labelL childAttLR
+-- attrib2get = hLookupByChild labelR childAttLR
+--  -- no instance
+--  -- attrib2g = hLookupByChild label3 childAttLR
+
+-- test_update_ChAtts_1 = updateAtChild labelL attrib3 childAttLR
+-- test_update_ChAtts_2 = updateAtChild labelR attrib3 childAttLR
+
+-- -- pch = Tagged True ::  Tagged  (LabelR,Int) Bool 
+-- testsd = singledef (undefined :: Proxy 'True )
+--                    (undefined:: Proxy 'True)
+--                    (Label :: Label Label3)
+--                    pch childAttLR  
+
+-- --example explained:
+-- --ChAttsRec
+-- -- '['(LabelL, '['(Label2, Char)]),
+-- --   '(LabelR, '['(Label1, Int), '(Label2, Char)])]
+-- --then add on child LabelR an attibute called Label3 with type Bool : 
+
+-- --ChAttsRec
+-- --  '['(LabelL, '['(Label2, Char)]),
+-- --    '(LabelR, '['(Label3, Bool), '(Label1, Int), '(Label2, Char)])]
+
+
+-- nts :: HList [Bool, Int,Char]
+-- nts = undefined
+
+-- testdefs1 = defs (Label :: Label Label3)
+--                   nts
+--                  (EmptyR)
+--                   childAttLR
+
+
+
+-- pch = Tagged True ::  Tagged  (LabelL,Char) Bool 
+-- pch2 = Tagged '2' :: Tagged (LabelR,Int) Char
+
+
+-- testdefs2 -- = undefined
+--   = defs (Label :: Label Label3)
+--     nts
+--     (ConsR pch (ConsR pch2 EmptyR))
+--     childAttLR
+
+
+
+-- --deriving instance (Show (Label l), Show v) =>  Show (TaggedChAtt l v)
+-- deriving instance  (Show (Label l), Show (Attribution a))
+--   => Show (TaggedChAttr l a)
+-- instance Show (Label Label1) where
+--   show _ = "label11"
+-- instance Show (Label Label2) where
+--   show _ = "label12"
+-- instance Show (Label Label3) where
+--   show _ = "label13"
+-- instance Show (Label Label4) where
+--   show _ = "label14"
+-- instance Show (Label LabelR) where
+--   show _ = "label1R"
+-- instance Show (Label LabelL) where
+--   show _ = "label1L"
+
+
+
+
+-- --RECORD
+
+
+-- --recall:
+-- --tagged1 = Tagged 3   :: Tagged Label1 Int 
+-- --tagged2 = Tagged '4' :: Tagged Label2 Char
+-- --tagged3 = Tagged '4' :: Tagged Label3 Char
+
+-- testrec = tagged1 *. tagged3 *. EmptyR
+-- t1 = hasLabelRec label2 testrec :: Proxy 'False
+-- t2 = hasLabelRec label3 testrec :: Proxy 'True
+
+
+-- --COM
+
+-- type ERule = Rule '[] '[] '[] '[] '[] '[]
+-- emptyRule = undefined :: Rule '[] '[] '[] '[] '[] '[]
+
+-- r1 :: Aspect [ '( Label2, ERule), '(Label1 ,ERule)]
+-- r1 = (Tagged emptyRule) `ConsR` ((Tagged emptyRule) `ConsR` EmptyR)
+
+-- r2 :: Aspect [ '( Label1, ERule), '(Label2 ,ERule)]
+-- r2 = (Tagged emptyRule) `ConsR` ((Tagged emptyRule) `ConsR` EmptyR)
+
+-- r3 = r1 .+. r2
